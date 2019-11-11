e7e745d964bda398bdf3e439178f3425efdfbd81
Irina <sidoren-irina@yandex.ru>
2019-11-12 00:50:36 +0300

    date and time picker add

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 var taskName
+$(function () {
+  $('#example').datetimepicker();
+});
 
 window.addEventListener("DOMContentLoaded", () => {
   taskName = document.querySelector('.task-name')
   taskName.oninput = function() {
-    
+
+
   }
 })
 
-function addText() {
-
-}
+  
